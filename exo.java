2ec77b5fb4359ddf9b9e2980e16fccd0b6ed10ae
swarna <mudulidipuku@gmail.com>
2023-04-28 01:39:49 +0300

    first commit

---
+++ exo.java
@@ -0,0 +1,10 @@
+import java.io.UnsupportedEncodingException;
+
+public class exo {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+       
+          String a="qqq";
+          byte[] b=a.getBytes();
+        System.out.println(b);
+    }
+}
